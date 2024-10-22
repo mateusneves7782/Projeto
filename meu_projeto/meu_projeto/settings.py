39564a1d1810a1546166dcf24645aca73e68e7d9
mateusneves7782 <mateusdemouraneves@gmail.com>
2024-10-22 16:18:14 -0300

    Update settings.py

--- meu_projeto/meu_projeto/settings.py
+++ meu_projeto/meu_projeto/settings.py
@@ -96,9 +96,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'unitaudealunosparaalunos',
-        'USER': 'root',
-        'PASSWORD': '02052006',
+        'NAME': 'Seu_Banco_De_Dados',
+        'USER': 'Seu_User',
+        'PASSWORD': 'Sua_Senha',
         'HOST': 'localhost',
         'PORT': '3306'
     }
